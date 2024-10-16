2c959a6d949f3b6e35153eb555bf02d8b4f9f743
lidenis <lidenis@pce-test>
2024-10-16 11:41:27 +0200

    ajout du js

---
+++ Site/script.js
@@ -0,0 +1,56 @@
+const priceRange = document.getElementById("price-range");
+const priceValue = document.getElementById("price-value");
+const rayonRange = document.getElementById("rayon-range");
+const rayonValue = document.getElementById("rayon-value");
+const notemin = document.getElementById('notemin');
+const notemax = document.getElementById('notemax');
+const datedeb = document.getElementById('datedeb');
+const datefin = document.getElementById('datefin');
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function adjustValues() {
+    let minValue = parseInt(notemin.value);
+    let maxValue = parseInt(notemax.value);
+
+    if (minValue > maxValue) {
+        notemax.value = minValue;
+    }
+}
+
+function adjustDates() {
+    const startDate = new Date(datedeb.value);
+    const endDate = new Date(datefin.value);
+
+    if (startDate > endDate) {
+        datefin.value = datedeb.value; 
+    }
+}
+
+document.getElementById("filterBtn").addEventListener("click", function() {
+    var filterForm = document.getElementById("filterForm");
+    if (filterForm.style.display === "none" || filterForm.style.display === "") {
+        filterForm.style.display = "block";
+    } else {
+        filterForm.style.display = "none";
+    }
+});
+
+priceRange.addEventListener("input", function() {
+    priceValue.textContent = priceRange.value;
+});
+
+rayonRange.addEventListener("input", function() {
+    rayonValue.textContent = rayonRange.value;
+});
+
+notemin.addEventListener('change', adjustValues);
+notemax.addEventListener('change', adjustValues);
+datedeb.addEventListener('change', adjustDates);
+datefin.addEventListener('change', adjustDates);
