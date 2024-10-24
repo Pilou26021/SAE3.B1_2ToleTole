5e170904911475504435e23bbb194c608bb45bf0
NEVOT <24657574+Pilou26021@users.noreply.github.com>
2024-10-24 23:26:27 +0200

    correction erreur script connection

--- SQL/connection_envdev.php
+++ SQL/connection_envdev.php
@@ -1,6 +1,6 @@
 <?php 
-// on va get les variables d'environnement depuis le fichier .env
-$servername = 'dbadmin-tole-tole.ventsdouest.dev';
+// On va get les variables d'environnement depuis le fichier .env
+$host = 'dbadmin-tole-tole.ventsdouest.dev'; // anciennement $servername
 $username = getenv('DB_USER');
 $password = getenv('DB_ROOT_PASSWORD');
 $dbname = getenv('DB_NAME');
@@ -11,8 +11,8 @@ $driver = "pgsql";
 try {
     // Création d'une nouvelle instance PDO
     $dsn = "$driver:host=$host;port=$port;dbname=$dbname;";
-    $conn = new PDO($dsn, $user, $password);
-    
+    $conn = new PDO($dsn, $username, $password); // ici on utilise $username
+
     // Définir le mode d'erreur de PDO sur Exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
@@ -20,7 +20,7 @@ try {
     die("Connection failed: " . $e->getMessage());
 }
 
-// On se place sur le schema sae
+// On se place sur le schema sae (ou 'public')
 $sql = "SET SCHEMA 'public';";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
