6766de77bd6b6e3363bd65c9016483cc91ab7b13
NEVOT <24657574+Pilou26021@users.noreply.github.com>
2024-12-02 20:08:40 +0100

    Redirect vers la page de connexion membre

--- Site/creer_compte_membre.php
+++ Site/creer_compte_membre.php
@@ -388,8 +388,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php if ($step === 4): ?>
         <div class="success">
             <h1>Votre compte a été créé avec succès !</h1>
-            <p>Vous pouvez maintenant vous connecter.</p>
-            <a href="connexion_membre.php" class="submit-btn">Se connecter</a>
+            <>Vous allez être redirigé vers la page de connexion...</p>
+            <script>
+                setTimeout(() => {
+                    window.location.href = 'connexion_membre.php';
+                }, 3000);
+            </script>
         </div>
     <?php endif; ?>
 
