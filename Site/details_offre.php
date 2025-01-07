42bcccade9fe8b3e8845ef4dbc1ffc371f051a1c
NEVOT <24657574+Pilou26021@users.noreply.github.com>
2025-01-07 16:44:31 +0100

    test

--- Site/details_offre.php
+++ Site/details_offre.php
@@ -3,9 +3,11 @@
     include "header.php";
     include "../SQL/connection_local.php";
 
+    /*
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+    */
 
     $professionel = false;
     $bonProfessionnel = false;
