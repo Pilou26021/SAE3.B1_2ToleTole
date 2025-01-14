b8a73665413d9a3548a49885fce566eed8325d86
NEVOT <24657574+Pilou26021@users.noreply.github.com>
2025-01-14 14:04:32 +0100

    bdd + page securite (en cours)

--- Site/creer_compte_pro.php
+++ Site/creer_compte_pro.php
@@ -220,7 +220,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $cleapi = generateAPIKey("professionel", $conn);
             
             // Ajout de la clé API
-            $stmt = $conn->prepare("UPDATE _compte SET chat_cleapi = ? WHERE idCompte = ?");
+            $stmt = $conn->prepare("UPDATE _compte SET chat_cleapi = ? WHERE idcompte = ?");
             $stmt->bindValue(1, $cleapi, PDO::PARAM_STR); 
             $stmt->bindValue(2, $idCompte, PDO::PARAM_INT);
             $stmt->execute();
---
+++ Site/securite.php
@@ -0,0 +1,78 @@
+<?php
+
+    ob_start();
+
+    include "header.php";
+    include "../SQL/connection_local.php";
+
+    // On récupère l'id du compte
+    $idPro = $_SESSION['idpro'];
+    $stmt = $conn->prepare("SELECT idCompte FROM _professionnel WHERE idPro = ?");
+    $stmt->execute(array($idPro));
+    $idCompte = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="./style.css">   
+    <title>Sécurité</title>
+</head>
+    <body>
+        <!-- Flèche de retour à la page mon_compte -->
+        <div style=" position:sticky; top:20px; left:20px; width: 100%;">
+                <a style="text-decoration: none; font-size: 30px; color: #040316; cursor: pointer;" href="./mon_compte.php">&#8617;</a>
+        </div>
+        
+        <main class = "securite_main">
+            <!-- Modification du mot de passe -->
+            <div class="securite_div">
+                <h2>Modifier le mot de passe</h2>
+                <form action="./securite.php" method="post">
+                    <label for="mdp">Mot de passe actuel</label>
+                    <input type="password" id="mdp" name="mdp" required>
+                    <br>
+                    <label for="mdp1">Nouveau mot de passe</label>
+                    <input type="password" id="mdp1" name="mdp1" required>
+                    <br>
+                    <label for="mdp2">Confirmer le mot de passe</label>
+                    <input type="password" id="mdp2" name="mdp2" required>
+                    <input type="submit" value="Modifier">
+                </form>
+            </div>
+            
+            <div class="securite_div">
+                <!-- Clé API dans une zone flouté and fait un appelle à la bdd pour la récupérer -->
+                    <h2>Clé API</h2>
+                    <div class="cleapi">
+                        <p></p>
+                    </div>
+                    <?php
+                        // Vérifie que la clé API n'a jamais été révélée
+                        $stmt = $conn->prepare("SELECT chat_cledevoile FROM _compte WHERE idCompte = ?");
+                        $stmt->execute(array($_SESSION['idCompte']));
+                        $chat_cledevoile = $stmt->fetch(PDO::FETCH_ASSOC);
+                        if($chat_cledevoile['chat_cledevoile'] == false){
+                            echo "<button id='btn_cleapi' onclick='afficher_cleapi()'>Afficher la clé API</button>";
+                        } else {
+                            echo "La clé API a déjà été révélée, veuillez la regénérer si vous la perdez.";
+                        }
+                        
+                    ?>
+            </div>
+        </main>
+        <div id="footer"></div>
+
+        <!-- Script pour header et footer -->
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script>
+            $(function() {
+                $("#footer").load("./footer.html");
+            });
+        </script>
+        <script src="./script.js" ></script>
+    </body>
+</html>
