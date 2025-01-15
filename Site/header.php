7b37da190f9562b7c78e7f60bf33e098c36526b0
MxPerrot <maxime.perrot@etudiant.univ-rennes1.fr>
2025-01-15 01:39:01 +0100

    ajout des notifs

--- Site/header.php
+++ Site/header.php
@@ -19,6 +19,7 @@ if (isset($_SESSION['membre'])) {
 }
 
 ?>
+
 <header>
     <nav id="mySidenav" class="sidenav">
         <div class="profil">
@@ -48,6 +49,7 @@ if (isset($_SESSION['membre'])) {
 
             <?php endif; ?>
         </div>
+
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <ul>
 
@@ -72,14 +74,167 @@ if (isset($_SESSION['membre'])) {
     </nav>
 
     <span class="openbtn" onclick="openNav()">&#9776;</span>
-    <a href="index.php"> <img src="img/logos/fond_remove_big.png" alt="logo site noir" title="logo site noir" style="width: 90px; height: 90x;"></a>
-    <?php if(!empty($professionel) || !empty($membre)) {
-        ?> <a href="mon_compte.php"> <?php
-    } else {
-        ?> <a href="connexion_membre.php"> <?php
-    } ?>
-        <img class="header-pdp-user" src="<?php echo (!empty($professionel) || !empty($membre)) ? $result['pathimage'] : './img/icons/user.png'; ?>" alt="image user" title="image user">
-    </a>
+    <a href="index.php"> <img src="img/logos/fond_remove_big.png" alt="logo site noir" title="logo site noir" style="width: 90px; height: 90x; margin-left: 40%;"></a>
+  
+
+    <!-- Partie notifications -->
+    <section class="header_partie_notif">
+
+        <?php if (!empty($professionel)){
+
+            $getNotifs = "SELECT *
+                          FROM _notification n
+                          WHERE n.idcompte = :idPro AND n.lu = false
+                          ORDER BY n.datenotification DESC";
+
+            $getNotifs = $conn->prepare($getNotifs);
+            $getNotifs->bindValue(':idPro', $idcompte, PDO::PARAM_INT);
+            $getNotifs->execute();
+            $notifications = $getNotifs->fetchAll();
+
+            //Compteur pour avoir le nombre d'avis non-lus
+            $compteur = 0;
+
+            foreach($notifications as $notif){
+                $compteur++;
+            }
+
+        ?>
+
+        <!-- Affichage du nombre d'avis non-lus -->
+        <p><?php
+            echo $compteur;
+            ?></p>
+        
+        <!-- Icon de notifications -->
+        <img class="header-pdp-user" src="
+        <?php if ($notifications != false){
+            ?>../img/icons/notifs_true.png<?php
+        }
+        else{?>../img/icons/notifs_false.png<?php
+        }?>" id="myBtn" alt="notifications" title="Mes notifications">
+            
+        <!-- Le Modal -->
+        <div id="myModal" class="notif_modal">
+        
+          <!-- Surmodal pour cacher le débordement de la barre de scroll -->
+            <div class="notif_surmodal">
+                <div class="notif_modal-content">
+                    <!-- Croix pour fermer le modal -->
+                    <span class="notif_close">&times;</span>
+
+                    <?php
+                        if ($notifications != NULL){
+
+                            echo "<h3 style='text-align: center;'>Vous avez " . $compteur . " avis non-lu(s) !</h3>";
+
+                            $dateDepart = Null;
+
+                            foreach($notifications as $notif){
+                                if (($notif["datenotification"] < $dateDepart) OR ($dateDepart == Null)){
+                                
+                                    $dateDepart = $notif["datenotification"];
+
+                                    $timestamp = strtotime($notif["datenotification"]);
+
+                                    // Tableau associatif pour traduire les mois en français
+                                    $months = [
+                                        'January' => 'janvier',
+                                        'February' => 'février',
+                                        'March' => 'mars',
+                                        'April' => 'avril',
+                                        'May' => 'mai',
+                                        'June' => 'juin',
+                                        'July' => 'juillet',
+                                        'August' => 'août',
+                                        'September' => 'septembre',
+                                        'October' => 'octobre',
+                                        'November' => 'novembre',
+                                        'December' => 'décembre'
+                                    ];
+
+
+                                    $formatted_date = date('d F Y', $timestamp);
+
+                                    $english_month = date('F', $timestamp);
+                                    $formatted_date = str_replace($english_month, $months[$english_month], $formatted_date);
+
+                                    echo "<h5>" . $formatted_date . "</h5>";
+                                    
+                                }
+
+                                echo "<p class='notif_comment'>" . $notif["messagenotification"] . "</p>";
+                            }
+
+                            // On met les avis en "lu"
+                            // $mettreEnLu = "UPDATE _notification
+                            //             SET lu = true
+                            //             WHERE idcompte = :idPro AND lu = false";
+
+                            // $mettreEnLu = $conn->prepare($mettreEnLu);
+                            // $mettreEnLu->bindValue(':idPro', $idcompte, PDO::PARAM_INT);
+                            // $mettreEnLu->execute();
+
+                        }
+                        else{
+                            echo "<h3 style='text-align: center;'> Vous n'avez aucune nouvelle notification </h3>";
+                        }
+
+                    ?>
+                </div>
+            </div>
+        </div>
+        
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+
+                var modal = document.getElementById("myModal");
+                var btn = document.getElementById("myBtn");
+                var span = document.getElementsByClassName("notif_close")[0];
+
+                btn.onclick = function() {
+                    // Si le modal est déjà visible, le cacher, sinon l'afficher
+                    if (modal.style.display === "block") {
+                        modal.style.display = "none";
+                        location.reload();
+                        
+                    } else {
+                        var rect = btn.getBoundingClientRect();
+                        var top = rect.top + window.scrollY;
+                        var left = rect.left + window.scrollX;
+
+                        // Positionnement du modal
+                        modal.style.top = (top) + 50 + 'px';
+                        modal.style.right = '95px';
+
+                        modal.style.display = "block";
+                    }
+                };
+
+                // Lorsque l'utilisateur clique sur la croix pour fermer le modal
+                span.onclick = function() {
+                    modal.style.display = "none";
+                    location.reload();
+
+                };
+
+            });
+        </script>
+
+        <?php
+        }?>
+
+        <?php if(!empty($professionel) || !empty($membre)) {
+            ?> <a href="mon_compte.php"> <?php
+        } else {
+            ?> <a href="connexion_membre.php"> <?php
+        } ?>
+            <img class="header-pdp-user" src="<?php echo (!empty($professionel) || !empty($membre)) ? $result['pathimage'] : './img/icons/user.png'; ?>" alt="image user" title="image user">
+        </a>
+    </section>
+
+    
+
 </header>
 <?php
 ob_end_flush();
