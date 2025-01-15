74eeaa65f0badba8a21dbd858d8f01db8be300b4
MxPerrot <maxime.perrot@etudiant.univ-rennes1.fr>
2025-01-15 20:03:27 +0100

    maj

---
+++ Site/mise_en_lu.php
@@ -0,0 +1,26 @@
+<?php
+// Création de la session
+
+error_reporting(0);
+
+ob_start();
+session_start();
+
+include "../SQL/connection_local.php";   
+
+// On vérifie si l'utilisateur est connecté. Il peut être connecté en tant que membre ou professionnel. Si il n'est pas connecté alors il sera visiteur.
+
+$idcompte = $_SESSION['professionnel'];
+
+// On met les avis en "lu"
+$mettreEnLu = "UPDATE _notification
+               SET lu = true
+               WHERE idcompte = :idPro AND lu = false";
+
+$mettreEnLu = $conn->prepare($mettreEnLu);
+$mettreEnLu->bindValue(':idPro', $idcompte, PDO::PARAM_INT);
+$mettreEnLu->execute();
+
+header("Location: avis_mes_offres.php");
+
+?>
