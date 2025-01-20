2bcd8b2326f93229013a6443b2e876a9e60a254a
NEVOT <24657574+Pilou26021@users.noreply.github.com>
2025-01-20 08:25:05 +0100

    Fix clé api pour membre

--- Site/securite.php
+++ Site/securite.php
@@ -12,12 +12,20 @@
         exit();
     }
 
-    // On récupère l'id du compte
-    $idPro = $_SESSION['idpro'];
-    $stmt = $conn->prepare("SELECT idcompte FROM _professionnel WHERE idpro = ?");
-    $stmt->bindParam(1, $idPro, PDO::PARAM_INT);
-    $stmt->execute();
-    $idCompte = $stmt->fetch(PDO::FETCH_ASSOC)['idcompte'];
+    if (isset($_SESSION['membre'])) {
+        $id = $_SESSION['idmembre'];
+        $stmt = $conn->prepare("SELECT idcompte FROM _membre WHERE idmembre = ?");
+        $stmt->bindParam(1, $_SESSION['membre'], PDO::PARAM_INT);
+        $stmt->execute();
+        $idCompte = $stmt->fetch(PDO::FETCH_ASSOC); 
+    } elseif (isset($_SESSION['professionnel'])) {
+        $id = $_SESSION['idpro'];
+        $stmt = $conn->prepare("SELECT idcompte FROM _professionnel WHERE idpro = ?");
+        $stmt->bindParam(1, $_SESSION['professionel'], PDO::PARAM_INT);
+        $stmt->execute();
+        $idCompte = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    $idCompte = $idCompte['idcompte'];
 
     $professionel = false;
     $membre = false;
@@ -46,6 +54,148 @@
                 <a style="text-decoration: none; font-size: 30px; color: #040316; cursor: pointer;" href="./mon_compte.php">&#8617;</a>
         </div>
         
+        <script>
+            function verifierComplexiteMdp() {
+                const mdp1 = document.getElementById("mdp1").value;
+                const message = document.getElementById("complexiteMessage");
+                const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+                if (mdp1 === "") {
+                    message.textContent = "";
+                } else if (!regex.test(mdp1)) {
+                    message.textContent = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre.";
+                    message.style.color = "red";
+                } else {
+                    message.textContent = "Mot de passe valide.";
+                    message.style.color = "green";
+                }
+            }
+            function verifierCorrespondanceMdp() {
+                const mdp1 = document.getElementById("mdp1").value;
+                const mdp2 = document.getElementById("mdp2").value;
+                const message = document.getElementById("correspondanceMessage");
+                if (mdp2 === "") {
+                    message.textContent = "";
+                } else if (mdp1 !== mdp2) {
+                    message.textContent = "Les mots de passe ne correspondent pas.";
+                    message.style.color = "red";
+                } else {
+                    message.textContent = "Les mots de passe correspondent.";
+                    message.style.color = "green";
+                }
+            }
+            function verifierMotDePasse() {
+                const mdp1 = document.getElementById("mdp1").value;
+                const mdp2 = document.getElementById("mdp2").value;
+                const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+                if (!regex.test(mdp1)) {
+                    alert("Le mot de passe ne respecte pas les critères de sécurité.");
+                    return false;
+                }
+                if (mdp1 !== mdp2) {
+                    alert("Les mots de passe ne correspondent pas.");
+                    return false;
+                }
+                // Requete pour changer le mot de passe
+                valideMotDePasse(function(success) {
+                    if (success) {
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("POST", "./api/changer_mdp.php", true);
+                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                        xhr.send("mdp=" + mdp1 + "&idcompte=" + <?php echo $idCompte; ?>);
+                        // Alerte l'utilisateur que le mot de passe a été modifié et raffaichie la page quand il clique sur ok
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState === 4 && xhr.status === 200) {
+                                alert("Le mot de passe a été modifié.");
+                                location.reload();
+                            }
+                        };
+                    } else {
+                        alert("Le mot de passe actuel est incorrect.");
+                    }
+                });
+            }
+
+            function valideMotDePasse(callback) {
+                var mdp = document.getElementById("mdp").value;
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "./api/valider_mdp.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            var response = JSON.parse(xhr.responseText);
+                            callback(response.success); // Exécute le callback avec true ou false.
+                        } else {
+                            callback(false); // En cas d'erreur serveur, retourne false.
+                        }
+                    }
+                };
+                xhr.send("mdp=" + encodeURIComponent(mdp) + "&idcompte=" + <?php echo $idCompte; ?>);
+            }
+                    
+            // Fonction pour regénérer la clé API
+            function regenerer_cleapi() {
+                var cleapi = "<?php echo generateAPIKey($typecompte, $conn); ?>";
+                document.querySelector(".cleapi p").innerHTML = 'xxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+                document.querySelector(".cleapi").classList.add("floutage");
+                changer_cleapi(cleapi);
+            }
+                
+            // Fonction pour afficher la clé API
+            function afficher_cleapi() {
+                cleapi_devoile(function (cleEstDevoile) {
+                    if (cleEstDevoile == 0) {
+                        get_cleapi(function (cleapi) {
+                        document.querySelector(".cleapi p").innerHTML = cleapi;
+                            document.querySelector(".cleapi").classList.remove("floutage");
+                            devoiler_cleapi();
+                        });
+                    } else {
+                        document.querySelector(".cleapi p").innerHTML = "La clé API a déjà été dévoilée.";
+                        document.querySelector(".cleapi").classList.remove("floutage");
+                    }
+                });
+            }
+            
+            function changer_cleapi(cleapi) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "./api/changer_cleapi.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send("cleapi=" + cleapi + "&idcompte=" + <?php echo $idCompte; ?>);
+            }
+                
+            function devoiler_cleapi() {
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "./api/devoiler_cleapi.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send("idcompte=" + <?php echo $idCompte; ?>);
+            }
+                
+            function get_cleapi(callback) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "./api/get_cleapi.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send("idcompte=" + <?php echo $idCompte; ?>);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        callback(xhr.responseText);
+                    }
+                };
+            }
+                
+            function cleapi_devoile(callback) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "./api/cleapi_devoile.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send("idcompte=" + <?php echo $idCompte; ?>);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        callback(xhr.responseText);
+                    }
+                };
+            }
+        </script>
+
         <main class = "securite_main">
             <!-- Modification du mot de passe -->
             <div class="securite_div <?php echo $professionel ? 'professionnel' : ($membre ? 'membre' : 'guest'); ?>">
@@ -69,159 +219,9 @@
                 </section>
             </div>
 
-            <script>
-                function verifierComplexiteMdp() {
-                    const mdp1 = document.getElementById("mdp1").value;
-                    const message = document.getElementById("complexiteMessage");
-                    const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
-
-                    if (mdp1 === "") {
-                        message.textContent = "";
-                    } else if (!regex.test(mdp1)) {
-                        message.textContent = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre.";
-                        message.style.color = "red";
-                    } else {
-                        message.textContent = "Mot de passe valide.";
-                        message.style.color = "green";
-                    }
-                }
-
-                function verifierCorrespondanceMdp() {
-                    const mdp1 = document.getElementById("mdp1").value;
-                    const mdp2 = document.getElementById("mdp2").value;
-                    const message = document.getElementById("correspondanceMessage");
-
-                    if (mdp2 === "") {
-                        message.textContent = "";
-                    } else if (mdp1 !== mdp2) {
-                        message.textContent = "Les mots de passe ne correspondent pas.";
-                        message.style.color = "red";
-                    } else {
-                        message.textContent = "Les mots de passe correspondent.";
-                        message.style.color = "green";
-                    }
-                }
-
-                function verifierMotDePasse() {
-                    const mdp1 = document.getElementById("mdp1").value;
-                    const mdp2 = document.getElementById("mdp2").value;
-                    const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
-
-                    if (!regex.test(mdp1)) {
-                        alert("Le mot de passe ne respecte pas les critères de sécurité.");
-                        return false;
-                    }
-                    if (mdp1 !== mdp2) {
-                        alert("Les mots de passe ne correspondent pas.");
-                        return false;
-                    }
-                    // Requete pour changer le mot de passe
-                    valideMotDePasse(function(success) {
-                        if (success) {
-                            var xhr = new XMLHttpRequest();
-                            xhr.open("POST", "./api/changer_mdp.php", true);
-                            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                            xhr.send("mdp=" + mdp1 + "&idcompte=" + <?php echo $idCompte; ?>);
-                            // Alerte l'utilisateur que le mot de passe a été modifié et raffaichie la page quand il clique sur ok
-                            xhr.onreadystatechange = function() {
-                                if (xhr.readyState === 4 && xhr.status === 200) {
-                                    alert("Le mot de passe a été modifié.");
-                                    location.reload();
-                                }
-                            };
-                        } else {
-                            alert("Le mot de passe actuel est incorrect.");
-                        }
-                    });
-                }
-
-            function valideMotDePasse(callback) {
-                var mdp = document.getElementById("mdp").value;
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", "./api/valider_mdp.php", true);
-                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState === 4) {
-                        if (xhr.status === 200) {
-                            var response = JSON.parse(xhr.responseText);
-                            callback(response.success); // Exécute le callback avec true ou false.
-                        } else {
-                            callback(false); // En cas d'erreur serveur, retourne false.
-                        }
-                    }
-                };
-                xhr.send("mdp=" + encodeURIComponent(mdp) + "&idcompte=" + <?php echo $idCompte; ?>);
-            }
-
-            </script>
-
             </div>
             <div class="securite_div <?php echo $professionel ? 'professionnel' : ($membre ? 'membre' : 'guest'); ?>">
                 <h2>Clé API</h2>
-                <script>
-                    // Fonction pour regénérer la clé API
-                    function regenerer_cleapi() {
-                        var cleapi = "<?php echo generateAPIKey($typecompte, $conn); ?>";
-                        document.querySelector(".cleapi p").innerHTML = 'xxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-                        document.querySelector(".cleapi").classList.add("floutage");
-                        changer_cleapi(cleapi);
-                    }
-
-                    // Fonction pour afficher la clé API
-                    function afficher_cleapi() {
-                        cleapi_devoile(function (cleEstDevoile) {
-                            if (cleEstDevoile == 0) {
-                                get_cleapi(function (cleapi) {
-                                    document.querySelector(".cleapi p").innerHTML = cleapi;
-                                    document.querySelector(".cleapi").classList.remove("floutage");
-                                    devoiler_cleapi();
-                                });
-                            } else {
-                                document.querySelector(".cleapi p").innerHTML = "La clé API a déjà été dévoilée.";
-                                document.querySelector(".cleapi").classList.remove("floutage");
-                            }
-                        });
-                    }
-
-                    function changer_cleapi(cleapi) {
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("POST", "./api/changer_cleapi.php", true);
-                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                        xhr.send("cleapi=" + cleapi + "&idcompte=" + <?php echo $idCompte; ?>);
-                    }
-
-                    function devoiler_cleapi() {
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("POST", "./api/devoiler_cleapi.php", true);
-                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                        xhr.send("idcompte=" + <?php echo $idCompte; ?>);
-                    }
-
-                    function get_cleapi(callback) {
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("POST", "./api/get_cleapi.php", true);
-                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                        xhr.send("idcompte=" + <?php echo $idCompte; ?>);
-                        xhr.onreadystatechange = function () {
-                            if (xhr.readyState == 4 && xhr.status == 200) {
-                                callback(xhr.responseText);
-                            }
-                        };
-                    }
-
-                    function cleapi_devoile(callback) {
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("POST", "./api/cleapi_devoile.php", true);
-                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                        xhr.send("idcompte=" + <?php echo $idCompte; ?>);
-                        xhr.onreadystatechange = function () {
-                            if (xhr.readyState == 4 && xhr.status == 200) {
-                                callback(xhr.responseText);
-                            }
-                        };
-                    }
-                </script>
-
                 <div class="cleapi floutage">
                     <p>xxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</p>
                 </div>
