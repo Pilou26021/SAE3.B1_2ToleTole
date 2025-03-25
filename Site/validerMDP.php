61b941d845b3c571d24b22a25893350449d6c982
EnzoEnzoC <enzocaz2004@gmail.com>
2025-03-25 17:29:52 +0100

    script pour verifier la validité du mot de passe

--- Site/validerMDP.php
+++ Site/validerMDP.php
@@ -1,52 +1,33 @@
 <?php
-session_start(); // Démarrer la session au tout début
+    session_start();
 
-include "../SQL/connection_local.php";
+    include "../SQL/connection_local.php";
 
-$idcompte = $_SESSION['membre'] ?? $_SESSION['professionnel'] ?? null;        
+    // On récupère l'id du compte
+    $idcompte = $_SESSION['membre'] ?? $_SESSION['professionnel'] ?? null;        
 
-//Récupération du mot de passe
-$requeteSql7 = "SELECT hashmdpcompte
-                FROM _compte
-                WHERE idcompte = :id;";
+    // On récupère le mot de passe lié au compte
+    $requeteSql = "SELECT hashmdpcompte
+                    FROM _compte
+                    WHERE idcompte = :id;";
 
-$executionRequete7 = $conn->prepare($requeteSql7);
-$executionRequete7->bindValue(':id', $idcompte, PDO::PARAM_INT);
-$executionRequete7->execute();
-$resusu = $executionRequete7 -> fetch();
+    $executionRequete = $conn->prepare($requeteSql);
+    $executionRequete->bindValue(':id', $idcompte, PDO::PARAM_INT);
+    $executionRequete->execute();
+    $resultat = $executionRequete -> fetch();
 
+    // On vérifie que le mot de passe saisi et le vrai mot de passe correspondent, et on renvoie une réponse en conséquence
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $mdpSaisi = htmlspecialchars($_POST['mdp']);
 
-    echo $resusu[0];
-    $nom = htmlspecialchars($_POST['texte']);
-
-    if (password_verify($nom, $resusu[0])){
-
-        unset($_POST['pswInput']);
-
-        ?>
-        <p style='color: red; font-size: 15px; margin: 0;'> Suppression de votre compte<p>
-        <?php
-
-        sleep(1);
-        header('Location: suppression_compte.php');
-        exit();
-
+        if (password_verify($mdpSaisi, $resultat[0])){
+            echo "MotDePasseValide";
+        }
+        else{
+            echo "MotDePasseNonvalide";
+        }
     }
-    else{
-
-        ?>
-        
-            <p style='color: red; font-size: 15px; margin: 0;'> Votre mot de passe est incorrect<p>
-            
-        <?php
-
-        var_dump($idcompte);
-
-    }
-}
-
 ?>
 
 
