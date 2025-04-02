47be9fd4451fc5f0e33ac3082313ebb451373d02
EnzoEnzoC <enzocaz2004@gmail.com>
2025-04-02 10:03:05 +0200

    ajout d'une popup de confirmation quand un compte vient d'etre supprime

--- Site/connexion_membre.php
+++ Site/connexion_membre.php
@@ -146,6 +146,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </section>
 </body>
 </html>
+
+<script>
+
+    document.addEventListener("DOMContentLoaded", function () {
+        setTimeout(() => {
+            const urlParams = new URLSearchParams(window.location.search);
+            if (urlParams.get("cpteSup") === "true") {
+                alert("Votre compte à bien été supprimé");
+            }
+        }, 1000);
+    });
+
+</script>
+
 <?php
 ob_end_flush();
 ?>
